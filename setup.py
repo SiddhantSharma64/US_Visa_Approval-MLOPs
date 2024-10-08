7df995c11c0757a267e58bcebc3c4b1f5ea03d81
SiddhantSharma64 <siddhantsharma6403@gmail.com>
2024-10-08 08:42:46 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Siddhant",
+    author_email="siddhantsharma6403@gmail.com",
+    packages=find_packages()
+)
